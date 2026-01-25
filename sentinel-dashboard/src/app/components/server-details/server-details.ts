35dfff8135ed7eb0bc9c4dae1d67ee8e86ce1170
pramu-k <pramukaluthanthri@gmail.com>
2026-01-25 17:04:38 +0530

    created components for dashborad and server details display

---
+++ sentinel-dashboard/src/app/components/server-details/server-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-details',
+  imports: [],
+  templateUrl: './server-details.html',
+  styleUrl: './server-details.css',
+})
+export class ServerDetails {
+
+}
