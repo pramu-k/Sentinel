4c406cd21916befb9a820c436e4c29c4b184cfef
pramu-k <pramukaluthanthri@gmail.com>
2026-01-26 22:33:05 +0530

    setup routing to server details component

--- sentinel-dashboard/src/app/components/server-details/server-details.ts
+++ sentinel-dashboard/src/app/components/server-details/server-details.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-server-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './server-details.html',
   styleUrl: './server-details.css',
 })
